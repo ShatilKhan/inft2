ac93d12329f0695b2c7a240f88e3407b1e61a845
ShatilKhan <shatilshahriar009@gmail.com>
2024-04-16 11:37:55 +0600

    6

--- index2.js
+++ index2.js
@@ -1,8 +1,16 @@
-
-document.getElementById('openFormButton2').addEventListener('click', function() {
+document.getElementById('openFormButton2').addEventListener('click', function(event) {
     document.getElementById('popupForm2').style.display = 'block';
+    event.stopPropagation();
+});
+
+document.getElementById('popupForm2').addEventListener('click', function(event) {
+    event.stopPropagation();
 });
 
 document.getElementById('closeFormButton2').addEventListener('click', function() {
+    document.getElementById('popupForm2').style.display = 'none';
+});
+
+document.addEventListener('click', function() {
     document.getElementById('popupForm2').style.display = 'none';
 });
