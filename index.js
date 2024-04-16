2e0e6aebbb59eea4d4aa2b2d41a5a202bcfecc76
Shahriar Shatil <52494840+ShatilKhan@users.noreply.github.com>
2024-04-16 07:43:43 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('openFormButton1').addEventListener('click', function() {
+    document.getElementById('popupForm1').style.display = 'block';
+});
+
+document.getElementById('closeFormButton1').addEventListener('click', function() {
+    document.getElementById('popupForm1').style.display = 'none';
+});
---
+++ index2.js
@@ -0,0 +1,8 @@
+
+document.getElementById('openFormButton2').addEventListener('click', function() {
+    document.getElementById('popupForm2').style.display = 'block';
+});
+
+document.getElementById('closeFormButton2').addEventListener('click', function() {
+    document.getElementById('popupForm2').style.display = 'none';
+});
